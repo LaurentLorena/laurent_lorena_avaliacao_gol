93aefd3ee15c6e375e939be4519da3385e3be337
Laurent Lorena <laurent.lorena@gmail.com>
2020-02-17 14:50:22 -0300

    Redux ok
    redux installed and working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {Router} from './services';
+import {Provider} from 'react-redux';
+import {Stores} from './stores';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 const App = () => {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.safe_area}>
-        <Router />
-      </SafeAreaView>
-    </NavigationContainer>
+    <Provider store={Stores}>
+      <NavigationContainer>
+        <SafeAreaView style={styles.safe_area}>
+          <Router />
+        </SafeAreaView>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
